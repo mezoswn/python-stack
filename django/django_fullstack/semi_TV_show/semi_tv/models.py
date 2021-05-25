ef94a2b971e849bcd8dc608ffccc834c2b723e2d
Moath <m.sweidan@hotmail.com>
2021-05-26 02:29:54 +0300

    Semi-Restful TV Shows

--- django/django_fullstack/semi_TV_show/semi_tv/models.py
+++ django/django_fullstack/semi_TV_show/semi_tv/models.py
@@ -1,6 +1,17 @@
 from django.db import models
-from django.db.models.fields import CharField, DateField, TextField, related
+from django.db.models.fields import CharField, DateField, TextField, related, datetime
 
+class BlogManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # add keys and values to errors dictionary for each invalid field
+        if len(postData['showtitle']) < 2:
+            errors["showtitle"] = "Show Title should be at least 2 characters"
+        if len(postData['network']) < 3:
+            errors["network"] = "Show Network should be at least 3 characters"
+        if len(postData['desc']) < 10:
+            errors["desc"] = "Show Description should be at least 10 characters"
+        return errors
 # Create your models here.
 class Show(models.Model):
     title = models.CharField(max_length=255)
